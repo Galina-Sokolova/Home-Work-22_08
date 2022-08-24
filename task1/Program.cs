51f6e34e97ff1c7357fd5b6296e499b850034981
GalinaS <peraskevasokolova@gmail.com>
2022-08-24 16:51:18 +0400

    вывод второй цифры 3-хзначного числа

---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿// программа принимает на вход случайно сгенерированное трёхзначное число и показывает вторую цифру этого числа
+int start = 100;
+int stop = 1000;
+
+int value = new Random().Next(start, stop);
+Console.WriteLine(value);
+
+int digit_two = value%100;
+int digit_second = digit_two/10;
+Console.Write("Вторая цифра случайного числа: "+digit_second);
