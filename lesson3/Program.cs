9eff57248e6816d85749540d13455ed2c3082b8a
GalinaS <peraskevasokolova@gmail.com>
2022-09-03 13:26:37 +0400

    нахождение числа в массиве

--- lesson3/Program.cs
+++ lesson3/Program.cs
@@ -1,26 +1,32 @@
-﻿int num = Convert.ToInt32(Console.ReadLine());
-int count = 0;
-int tmp = num;
-while (tmp > 0)
-{
-    int ost = tmp % 10;
-    if (ost % 2 == 0)
-    {
-      count++; 
-    }
-    tmp = tmp / 10;
-}
-Console.Write(count);
+﻿Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-int count = 0;
-int tmp = num;
-int sum = 0;
-while (tmp > 0)
+int N = 12;
+int [] arr = new int[N];
+Random rnd = new Random();
+Console.Write("Массив: ");
+for (int i = 0; i < N; i++)
 {
-    int ost = tmp % 10;
-    sum = sum + ost;
-    tmp = tmp / 10;
-    count++; 
-}    
-double avg = sum / count;
-Console.Write(count);
+  arr[i] = rnd.Next(-9, 10);
+  Console.Write(arr[i]+" ");
+}
+void finding(int num)
+{
+  int count = 0;
+  for (int i = 0; i < N; i++)
+  {
+    if (num == arr[i])
+    {
+      count++;
+    }
+  }
+  if (count == 0)
+  {
+    Console.WriteLine($"В данном массиве нет числа {num}");
+  }
+  else
+  {
+    Console.WriteLine($"Число {num} встречается в массиве {count} раз");
+  } 
+}
+Console.WriteLine("");
+finding(num); 
